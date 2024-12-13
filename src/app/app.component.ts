e738537d4172085f9ac074c4edeea14c28c9e272
vbl10 <99830114+vbl10@users.noreply.github.com>
2024-12-13 09:39:56 -0300

    Com objetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { FormsModule } from '@angular/forms';
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
-    public estrutura: Tree = treeFromObject({
+    public estrutura: any = {
         "pasta 1": {
             "pasta 2": {
                 "audio 1": "id",
@@ -29,28 +29,23 @@ export class AppComponent {
         "audio 8": "id",
         "audio 9": "id",
         "audio 10": "id"
-    });
-
-    public explorando: Tree = this.estrutura;
+    };
+    public explorando: any = this.estrutura;
     public caminho: string = '/';
-    public selecao = new Map<string, undefined>;
+    public selecao = new Map<string, any>;
     public novaPasta: string = '';
     public criandoNovaPasta: boolean = false;
 
-    constructor() {
-        console.log(this.estrutura);
-    }
-
     criarNovaPasta() {
         console.log('submetido');
-        if (!this.explorando.has(this.novaPasta)) {
-            this.explorando.set(this.novaPasta, treeFromObject({}));
+        if (!Object.hasOwn(this.explorando, this.novaPasta)) {
+            this.explorando[this.novaPasta] = {};
             this.novaPasta = '';
             this.criandoNovaPasta = false;
         }
     }
     novaPastaInvalida() {
-        return this.explorando.has(this.novaPasta);
+        return Object.hasOwn(this.explorando, this.novaPasta);
     }
 
     alterarCaminho() {
@@ -58,47 +53,24 @@ export class AppComponent {
     }
 
     pegarPastas() {
-        const pastas: string[] = [];
-        for (let item of this.explorando.entries())
-            if (typeof item[1] == 'object')
-                pastas.push(item[0]);
-        return pastas;
+        return Object.entries(this.explorando).filter(val => typeof val[1] == 'object').map(val => val[0]);
     }
     pegarAqruivos() {
-        const arquivos: string[] = [];
-        for (let item of this.explorando.entries())
-            if (typeof item[1] == 'string')
-                arquivos.push(item[0]);
-        return arquivos;
+        return Object.entries(this.explorando).filter(val => typeof val[1] == 'string').map(val => val[0]);
     }
 
     aoClicarItem(nome: string) {
         if (this.selecao.has(nome)) this.selecao.delete(nome);
-        else this.selecao.set(nome, undefined);
+        else this.selecao.set(nome, this.explorando[nome]);
     }
     selecionarTudo() {
         if (this.selecao.size > 0) this.selecao.clear();
-        else for (let item of this.explorando) this.selecao.set(item[0], undefined);
+        else this.selecao = new Map(Object.entries(this.explorando));
     }
 
     apagar() {
         for (let item of this.selecao.entries())
-            this.explorando.delete(item[0]);
+            delete this.explorando[item[0]];
         this.selecao.clear();
-        console.log(this.estrutura);
     }
-}
-
-type Tree = Map<string, string | Tree>;
-function treeFromObject(obj: any): Tree {
-    const tree: Tree = new Map<string, string | Tree>();
-    Object.entries(obj).forEach(val => {
-        if (typeof val[1] == 'string') {
-            tree.set(val[0], val[1]);
-        }
-        else {
-            tree.set(val[0], treeFromObject(val[1]));
-        }
-    })
-    return tree;
 }
